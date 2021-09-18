0b917fc99394bf2a80dda678d624224bff94d707
Thomas Depierre <depierre.thomas@gmail.com>
2021-09-18 16:26:34 +0200

    no i did not break the build

--- test/orion_web/live/page_live_test.exs
+++ test/orion_web/live/page_live_test.exs
@@ -5,7 +5,7 @@ defmodule OrionWeb.PageLiveTest do
 
   test "disconnected and connected render", %{conn: conn} do
     {:ok, page_live, disconnected_html} = live(conn, "/")
-    assert disconnected_html =~ "Orion"
-    assert render(page_live) =~ "Orion"
+    assert disconnected_html =~ "Run profiling"
+    assert render(page_live) =~ "Run profiling"
   end
 end
