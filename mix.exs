3cf615b008d8004e7c0914ee61638ca1bcbe9fb0
Thomas Depierre <depierre.thomas@gmail.com>
2021-09-18 14:44:21 +0200

    faster and not needed anymore

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule Orion.MixProject do
       version: "0.1.0",
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
-      compilers: [:phoenix, :gettext] ++ Mix.compilers(),
+      compilers: [:gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps()
