e3d8362b5bfc155b75dbdaa6a759fa9a5d8e8dac
Thomas Depierre <depierre.thomas@gmail.com>
2021-09-19 11:42:29 +0200

    we got fake data

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,6 +16,7 @@ import "phoenix_html"
 import { Socket } from "phoenix"
 import topbar from "topbar"
 import { LiveSocket } from "phoenix_live_view"
+import { ChartData } from "./chart.js"
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
 
@@ -23,7 +24,7 @@ let Hooks = {
   ChartData: ChartData
 }
 
-let liveSocket = new LiveSocket("/live", Socket, { params: { _csrf_token: csrfToken } })
+let liveSocket = new LiveSocket("/live", Socket, { hooks: Hooks, params: { _csrf_token: csrfToken } })
 
 // Show progress bar on live navigation and form submits
 topbar.config({ barColors: { 0: "#29d" }, shadowColor: "rgba(0, 0, 0, .3)" })
--- lib/orion_web/live/page_live.ex
+++ lib/orion_web/live/page_live.ex
@@ -6,11 +6,15 @@ defmodule OrionWeb.PageLive do
 
   @impl true
   def mount(_params, _session, socket) do
+    quantile_data = formatted_time_series([], "Linear")
+    scale = "Linear"
+
     data = %{
       match_spec: %MatchSpec{},
       fake_data: false,
-      quantile_data: "",
-      scale: ""
+      quantile_data: Jason.encode!(quantile_data),
+      quantile_data_raw: quantile_data,
+      scale: Jason.encode!(scale)
     }
 
     {:ok, assign(socket, data)}
@@ -20,7 +24,7 @@ defmodule OrionWeb.PageLive do
   def handle_event("query_submit", %{"match_spec" => query}, socket) do
     fake_data = query["fake_data"] == "true"
     # need to deal with this better?
-    quantile_data = formatted_time_series("Linear")
+    quantile_data = formatted_time_series([], "Linear")
 
     scale = "Linear"
 
@@ -32,55 +36,76 @@ defmodule OrionWeb.PageLive do
       },
       fake_data: fake_data,
       quantile_data: Jason.encode!(quantile_data),
+      quantile_data_raw: quantile_data,
       scale: Jason.encode!(scale)
     }
 
+    Process.send_after(self(), :update_data, 1_000)
     socket = assign(socket, data)
 
     {:noreply, socket}
   end
 
+  @five_minute_in_sec 5 * 60
+
+  @impl true
+  def handle_info(:update_data, socket) do
+    Process.send_after(self(), :update_data, 1_000)
+    quantile_data = formatted_time_series(socket.assigns.quantile_data_raw, "Linear")
+
+    data = %{
+      quantile_data: Jason.encode!(quantile_data),
+      quantile_data_raw: quantile_data
+    }
+
+    socket = assign(socket, data)
+    {:noreply, socket}
+  end
+
   defp get_fake_data() do
-    # fake some data
+    Enum.reduce(1..100, SimpleDog.new(), fn _x, acc ->
+      SimpleDog.insert(acc, :rand.uniform(1_000))
+    end)
   end
 
   # thx derek <3
   # https://github.com/spawnfest/beamwork
 
-  defp formatted_time_series("Linear") do
-    data = get_fake_data()
-    keys = Enum.map(data, fn {ts, _} -> ts end)
-    min_ts = Enum.min(keys, fn -> 0 end)
-    max_ts = Enum.max(keys, fn -> 0 end)
+  defp formatted_time_series([], "Linear") do
+    end_ts = System.os_time(:second)
 
-    keys = Enum.map(min_ts..max_ts, fn x -> x end)
+    start_ts =
+      DateTime.utc_now()
+      |> DateTime.add(-@five_minute_in_sec, :second)
+      |> DateTime.to_unix(:second)
+
+    keys = start_ts..end_ts
 
     [
-      keys,
-      Enum.map(keys, fn ts ->
-        get_quantile(data, ts, 0.99)
-      end),
-      Enum.map(keys, fn ts ->
-        get_quantile(data, ts, 0.90)
-      end),
-      Enum.map(keys, fn ts ->
-        get_quantile(data, ts, 0.50)
-      end),
-      Enum.map(keys, fn ts ->
-        Map.get(data, ts, SimpleDog.new()) |> SimpleDog.count() |> ceil()
-      end)
+      Enum.map(keys, fn x -> x end),
+      Enum.map(keys, fn _x -> 0 end),
+      Enum.map(keys, fn _x -> 0 end),
+      Enum.map(keys, fn _x -> 0 end),
+      Enum.map(keys, fn _x -> 0 end)
     ]
   end
 
-  defp get_quantile(data, ts, quantile) do
-    Map.get(data, ts, nil)
-    |> case do
-      nil ->
-        nil
+  defp formatted_time_series(old_data, "Linear") do
+    data = get_fake_data()
 
-      sd ->
-        val = SimpleDog.quantile(sd, quantile) |> ceil()
-        val / 1000
-    end
+    [timestamps, quantile99, quantile90, quantile50, count] = old_data
+
+    [
+      tl(timestamps) ++ [System.os_time(:second)],
+      tl(quantile99) ++ [get_quantile(data, 0.99)],
+      tl(quantile90) ++ [get_quantile(data, 0.90)],
+      tl(quantile50) ++ [get_quantile(data, 0.50)],
+      tl(count) ++ [data |> SimpleDog.count() |> ceil()]
+    ]
+  end
+
+  defp get_quantile(data, quantile) do
+    val = SimpleDog.quantile(data, quantile) |> ceil()
+    val / 1000
   end
 end
