71b5dcba747871ec6e2c329c8955493020ab5866
Thomas Depierre <depierre.thomas@gmail.com>
2021-09-19 21:09:55 +0200

    fix our only test

--- test/orion_web/live/page_live_test.exs
+++ test/orion_web/live/page_live_test.exs
@@ -5,7 +5,7 @@ defmodule OrionWeb.PageLiveTest do
 
   test "disconnected and connected render", %{conn: conn} do
     {:ok, page_live, disconnected_html} = live(conn, "/")
-    assert disconnected_html =~ "Run profiling"
-    assert render(page_live) =~ "Run profiling"
+    assert disconnected_html =~ "Run"
+    assert render(page_live) =~ "Run"
   end
 end
