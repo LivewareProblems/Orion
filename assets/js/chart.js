4470a30c8c1fe61484f247f37f22c9315f92a7f3
Eric Gisse <eric.gisse@gmail.com>
2021-09-19 03:44:40 -0500

    make liveview work kinda

---
+++ assets/js/chart.js
@@ -0,0 +1,149 @@
+import uPlot from 'uplot'
+
+let chart = null;
+
+function paths(u, sidx, i0, i1) {
+  const s = u.series[sidx];
+  const xdata = u.data[0];
+  const ydata = u.data[sidx];
+  const scaleX = 'x';
+  const scaleY = s.scale;
+
+  const stroke = new Path2D();
+
+  const x_width = Math.abs((u.valToPos(xdata[0], scaleX, true) - u.valToPos(xdata[1], scaleX, true)) / 2);
+
+  stroke.moveTo(
+    Math.round(u.valToPos(xdata[0], scaleX, true)),
+    Math.round(u.valToPos(ydata[0], scaleY, true))
+  );
+
+  for (let i = i0; i < i1; i++) {
+    let x0 = Math.round(u.valToPos(xdata[i], scaleX, true));
+    let y0 = Math.round(u.valToPos(ydata[i], scaleY, true));
+    let x1 = Math.round(u.valToPos(xdata[i + 1], scaleX, true));
+    let y1 = Math.round(u.valToPos(ydata[i + 1], scaleY, true));
+
+    stroke.lineTo(x0 - x_width, y0);
+    stroke.lineTo(x1 - x_width, y0);
+
+    if (i == i1 - 1) {
+      stroke.lineTo(x1 - x_width, y1);
+      stroke.lineTo(x1, y1);
+    }
+  }
+
+  const fill = new Path2D(stroke);
+
+  let minY = Math.round(u.valToPos(u.scales[scaleY].min, scaleY, true));
+  let minX = Math.round(u.valToPos(u.scales[scaleX].min, scaleX, true));
+  let maxX = Math.round(u.valToPos(u.scales[scaleX].max, scaleX, true));
+
+  fill.lineTo(maxX, minY);
+  fill.lineTo(minX, minY);
+
+  return {
+    stroke,
+    fill,
+  };
+}
+
+function safe_to_fixed(number, decimals) {
+  return number && number.toFixed(decimals)
+}
+
+function create_chart(data, scale) {
+  let rect = { width: window.innerWidth * 0.6, height: 400 };
+
+  let scaler = null;
+  if (scale == "Linear") {
+    scaler = (x) => x && x
+  } else if (scale == "Log10") {
+    scaler = (x) => x && Math.pow(10, x)
+  } else if (scale == "Log2") {
+    scaler = (x) => x && Math.pow(2, x)
+  }
+  let existing = document.getElementById("chart1");
+  existing && existing.remove();
+
+  let opts = {
+    title: "Module.function/arity timing",
+    id: "chart1",
+    class: "my-chart",
+    width: rect.width,
+    height: rect.height,
+    labelSize: 10,
+    labelFont: "bold 8px Arial",
+    ticks: { show: false },
+    points: { show: false },
+    font: "8px Arial",
+    series: [
+      { value: '{YYYY}-{MM}-{DD} {HH}:{mm}:{ss}' },
+      {
+        label: "P99",
+        stroke: "rgb(155, 214, 206)",
+        value: (self, rawValue) => safe_to_fixed(scaler(rawValue), 3) + "ms",
+        fill: "rgb(155, 214, 206, 0.5 )",
+        paths: paths,
+        scale: "ms"
+      },
+      {
+        label: "P90",
+        stroke: "rgb(79, 169, 184)",
+        value: (self, rawValue) => safe_to_fixed(scaler(rawValue), 3) + "ms",
+        fill: "rgb(79, 169, 184, 0.5)",
+        paths: paths,
+        scale: "ms"
+      },
+      {
+        label: "P50",
+        stroke: "rgb(2, 88, 115)",
+        value: (self, rawValue) => safe_to_fixed(scaler(rawValue), 3) + "ms",
+        fill: "rgb(2, 88, 115, 0.5)",
+        paths: paths,
+        scale: "ms"
+      },
+      {
+        label: "Throughput",
+        ke: "rgb(30, 30, 30)",
+        value: (self, rawValue) => scaler(rawValue) + "rpm",
+        scale: "rpm"
+      }
+    ],
+    axes: [
+      {},
+      {
+        scale: "ms",
+        grid: { show: false },
+        values: (u, vals, space) => vals.map((val) => safe_to_fixed(scaler(val), 2) + "ms")
+      },
+      {
+        side: 1,
+        values: (u, vals, space) => vals.map((val) => safe_to_fixed(scaler(val), 2) + "reqs"),
+        scale: "reqs",
+        grid: { show: false },
+      },
+    ]
+  };
+
+  chart = new uPlot(opts, data, document.getElementById("chart"));
+}
+
+let scale = "";
+
+export const ChartData = {
+  mounted() {
+    scale = JSON.parse(this.el.dataset.scale);
+    let quantile_data = JSON.parse(this.el.dataset.quantile);
+    create_chart(quantile_data, scale);
+  },
+  updated() {
+    let new_scale = JSON.parse(this.el.dataset.scale);
+    if (scale == new_scale) {
+      let quantile_data = JSON.parse(this.el.dataset.quantile);
+      chart.setData(quantile_data, scale);
+    } else {
+      this.mounted();
+    }
+  }
+}
--- lib/orion_web/live/page_live.ex
+++ lib/orion_web/live/page_live.ex
@@ -2,23 +2,85 @@ defmodule OrionWeb.PageLive do
   use OrionWeb, :live_view
 
   alias Orion.MatchSpec
+  alias DogSketch.SimpleDog
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, match_spec: %MatchSpec{})}
+    data = %{
+      match_spec: %MatchSpec{},
+      fake_data: false,
+      quantile_data: "",
+      scale: ""
+    }
+
+    {:ok, assign(socket, data)}
   end
 
   @impl true
-  def handle_event("match_spec", %{"query" => ms}, socket) do
-    socket =
-      assign(socket, %{
-        match_spec: %MatchSpec{
-          module_name: ms[:module_name],
-          function_name: ms[:function_name],
-          arity: ms[:arity]
-        }
-      })
+  def handle_event("query_submit", %{"match_spec" => query}, socket) do
+    fake_data = query["fake_data"] == "true"
+    # need to deal with this better?
+    quantile_data = formatted_time_series("Linear")
 
-    {:noreplay, socket}
+    scale = "Linear"
+
+    data = %{
+      match_spec: %MatchSpec{
+        module_name: query["module_name"],
+        function_name: query["function_name"],
+        arity: query["arity"]
+      },
+      fake_data: fake_data,
+      quantile_data: Jason.encode!(quantile_data),
+      scale: Jason.encode!(scale)
+    }
+
+    socket = assign(socket, data)
+
+    {:noreply, socket}
+  end
+
+  defp get_fake_data() do
+    # fake some data
+  end
+
+  # thx derek <3
+  # https://github.com/spawnfest/beamwork
+
+  defp formatted_time_series("Linear") do
+    data = get_fake_data()
+    keys = Enum.map(data, fn {ts, _} -> ts end)
+    min_ts = Enum.min(keys, fn -> 0 end)
+    max_ts = Enum.max(keys, fn -> 0 end)
+
+    keys = Enum.map(min_ts..max_ts, fn x -> x end)
+
+    [
+      keys,
+      Enum.map(keys, fn ts ->
+        get_quantile(data, ts, 0.99)
+      end),
+      Enum.map(keys, fn ts ->
+        get_quantile(data, ts, 0.90)
+      end),
+      Enum.map(keys, fn ts ->
+        get_quantile(data, ts, 0.50)
+      end),
+      Enum.map(keys, fn ts ->
+        Map.get(data, ts, SimpleDog.new()) |> SimpleDog.count() |> ceil()
+      end)
+    ]
+  end
+
+  defp get_quantile(data, ts, quantile) do
+    Map.get(data, ts, nil)
+    |> case do
+      nil ->
+        nil
+
+      sd ->
+        val = SimpleDog.quantile(sd, quantile) |> ceil()
+        val / 1000
+    end
   end
 end
